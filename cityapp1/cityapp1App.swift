2ae3c0b5900ea1334fd6a9225098a71dcec2299e
MJ <m.jamali2007@gmail.com>
2023-03-11 13:21:41 -0800

    Initial Commit

---
+++ cityapp1/cityapp1App.swift
@@ -0,0 +1,17 @@
+//
+//  cityapp1App.swift
+//  cityapp1
+//
+//  Created by Mohammad Jamali on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct cityapp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
